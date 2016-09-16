f0142ca5a3d0c87bf3b62e89b4cd902bc2a11ebb
Yoichi Hirai <i@yoichihirai.com>
2016-09-16 04:38:37 +0200

    prepare for codegen

--- src/run_tests.sh
+++ src/run_tests.sh
@@ -1,5 +1,5 @@
-ocamlbuild -Is ast,parse,lib -use-menhir parse/parser_test.native && \
-ocamlbuild -Is ast,parse,lib -use-menhir ast/ast_test.native && \
+ocamlbuild -Is ast,parse,lib,codegen -use-menhir parse/parser_test.native && \
+ocamlbuild -Is ast,parse,lib,codegen -use-menhir ast/ast_test.native && \
 for f in `ls parse/examples/*.sol`
 do
   echo "trying" $f
