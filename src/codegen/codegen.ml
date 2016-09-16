d79be064c192ca86449e336202f293d75cad520f
Yoichi Hirai <i@yoichihirai.com>
2016-09-16 19:56:40 +0800

    At least compilable

--- src/codegen/codegen.ml
+++ src/codegen/codegen.ml
@@ -14,19 +14,19 @@ let rec codegen_exp
       CodegenEnv.codegen_env =
   let ret =
   Syntax.
-  (match e with
-   | ValueExp ->
+  (match e,t with
+   | ValueExp,UintType ->
       let ce = CodegenEnv.append_instruction ce CALLVALUE in
       ce
-   | SenderExp ->
+   | SenderExp,AddressType ->
       let ce = CodegenEnv.append_instruction ce CALLER in
       ce
-   | ArrayAccessExp _ ->
+   | ArrayAccessExp _, _ ->
       failwith "code gen array access"
-   | ThisExp ->
+   | ThisExp,_ ->
       let ce = CodegenEnv.append_instruction ce ADDRESS in
       ce
-   | IdentifierExp id ->
+   | IdentifierExp id,_ ->
       begin match LocationEnv.lookup le id with
       (** if things are just DUP'ed, location env should not be
        * updated.  If they are SLOADED, the location env should be
@@ -36,50 +36,72 @@ let rec codegen_exp
          ce
       | None -> failwith ("identifier's location not found: "^id)
       end
-  | FalseExp ->
+  | FalseExp,BoolType ->
      let ce = CodegenEnv.append_instruction
                 ce (Evm.PUSH1 (Concrete Big_int.zero_big_int)) in
      ce
-  | TrueExp ->
+  | TrueExp,BoolType ->
      let ce = append_instruction ce (PUSH1 (Concrete Big_int.unit_big_int)) in
      ce
-  | NotExp sub ->
+  | NotExp sub,BoolType ->
      let ce = codegen_exp le ce sub in
      let ce = append_instruction ce NOT in
      ce
-  | NowExp ->
+  | NowExp,UintType ->
      append_instruction ce TIMESTAMP
-  | NeqExp (l, r)->
+  | NeqExp (l, r), BoolType ->
      let ce = codegen_exp le ce r in
      let ce = codegen_exp le ce l in (* l later because it should come at the top *)
      let ce = append_instruction ce NEQ in
      ce
-  | LtExp (l, r) ->
+  | LtExp (l, r), BoolType ->
      let ce = codegen_exp le ce r in
      let ce = codegen_exp le ce l in
      let ce = append_instruction ce LT in
      ce
-  | GtExp (l, r) ->
+  | GtExp (l, r), BoolType ->
      let ce = codegen_exp le ce r in
      let ce = codegen_exp le ce l in
      let ce = append_instruction ce GT in
      ce
-  | EqualityExp (l, r) ->
+  | EqualityExp (l, r), BoolType ->
      let ce = codegen_exp le ce r in
      let ce = codegen_exp le ce l in
      let ce = append_instruction ce EQ in
      ce
-  | SendExp s ->
+  | SendExp s, _ ->
+     failwith "codegen for SendExp"
      (* argument order, gas, to, value, in offset in size out offset, out size *)
-     failwith "exp code gen for sendexp"
-  | NewExp new_e ->
-     failwith "exp code gen for new expression, first maybe send"
-  | FunctionCallExp _ ->
+(*     let out_size = send_out_size t s in
+     let out_offset = usual_memory_offset in (* new location should be allocated *)
+     let in_size = sned_in_size s in
+     let in_offset = usual_memory_offset in
+     let ce = append_instruction ce (PUSH32 out_size) in
+     let ce = append_instruction ce (PUSH32 out_offset) in
+     let ce = append_instruction ce (PUSH32 in_size) in
+     let ce = append_instruction ce (PUSH32 in_offset) in
+     let ce = prepare_arguments_in_memory ce s in
+     let ce = codegen_exp ce value_exp in
+     let ce = codegen_exp ce to_exp in
+     let ce = append_instruction ce GAS in
+     let ce = append_instruction ce CALL in
+     let ce = append_instruction ce ISZERO in
+     let ce = append_instruction ce NOT in (* stack head should be true iff success *)
+     let (ce, success_label) = generate_new_label ce in
+     let ce = append_instruction ce (PUSH32 success_label) in (* jump destination in case of success *)
+     let ce = append_instruction ce IJUMP in
+     let ce = append_instruction ce (PUSH1 0) in
+     let ce = append_instruction ce JUMP in
+     let ce = append_instruction ce (JUMPDEST success_label) in
+     ce *)
+  | NewExp new_e, _ ->
+     failwith "exp code gen for new expression"
+  | FunctionCallExp _, _ ->
      (* TODO maybe the name callexp should be changed, the only instance is the newly created contract, for which the new_exp should be responsible *)
      failwith "exp code gen for callexp"
-  | ParenthExp _ ->
+  | ParenthExp _, _ ->
      failwith "ParenthExp not expected."
-  | AddressExp e ->
+  | AddressExp e, AddressType ->
      (* e is a contract instance.
       * The concrete representation of a contact instance is
       * already the address
