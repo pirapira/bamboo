971fc339ae4a3362f7f79ac4484b3322290bd682
Yoichi Hirai <i@yoichihirai.com>
2016-10-04 16:16:28 +0200

    Add another test of method signature hashing

--- src/lib/lib_test.ml
+++ src/lib/lib_test.ml
@@ -7,8 +7,18 @@ let case1_case : Syntax.usual_case_header =
   ; case_arguments = [{arg_typ = AddressType; arg_ident = "addr"}]
   })
 
+let case2_case : Syntax.usual_case_header =
+  Syntax.(
+    { case_return_typ = [UintType]
+    ; case_name = "f"
+    ; case_arguments = [{arg_typ = UintType; arg_ident = "x"}]
+    })
+
+let case2_hash :string = "b3de648b"
+
 let _ =
   let () = Printf.printf "case0 %s %s\n" (Ethereum.keccak_signature (fst case0)) (snd case0) in
   let () = assert (Ethereum.keccak_signature (fst case0) = (snd case0)) in
   let () = assert (Ethereum.case_header_signature_hash case1_case = (snd case0)) in
+  let () = assert ((Ethereum.case_header_signature_hash case2_case) = case2_hash) in
   Printf.printf "lib_test: success\n"
