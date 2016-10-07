5be6005cec8810ac97c2cae4d13badef7433e5f1
Yoichi Hirai <i@yoichihirai.com>
2016-10-07 11:49:47 +0200

    Add a new test file hex_test

--- src/run_tests.sh
+++ src/run_tests.sh
@@ -2,9 +2,11 @@ ocamlbuild -Is ast,parse,lib,codegen -package cryptokit -use-menhir parse/parser
 ocamlbuild -Is ast,parse,lib,codegen -package cryptokit -use-menhir ast/ast_test.native && \
 ocamlbuild -Is ast,parse,lib,codegen -package batteries -package cryptokit -use-menhir codegen/codegen_test.native && \
 ocamlbuild -Is ast,parse,lib,codegen -package batteries -package cryptokit -use-menhir codegen/codegen_test2.native && \
+ocamlbuild -Is ast,parse,lib,codegen -package batteries -package cryptokit -use-menhir codegen/hex_test.native && \
 ocamlbuild -Is ast,parse,lib,codegen -package batteries -package cryptokit -use-menhir lib/lib_test.native && \
 ./codegen_test.native || exit 1
 ./lib_test.native || exit 1
+./hex_test.native || exit 1
 for f in `ls parse/examples/*.sol`
 do
   echo "trying" $f
