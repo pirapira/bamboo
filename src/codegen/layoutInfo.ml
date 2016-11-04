6457db3f78b0e86ae0c8f74c0c08ba6147888b70
Yoichi Hirai <i@yoichihirai.com>
2016-11-04 15:59:47 +0100

    Think more about the code/storage layout

--- src/codegen/layoutInfo.ml
+++ src/codegen/layoutInfo.ml
@@ -8,14 +8,14 @@ type layout_info =
   ; contract_offset_in_runtime_code : Syntax.contract_id -> int
 
     (* And then, the runtime code is organized like this: *)
-    (* |dispatcher that jumps into a contract|runtime code for contract A|runtime code for contract B|runtime code for contract C| *)
+    (* |dispatcher that jumps into the current pc|dispatcher that jumps into current contract|runtime code for contract A|runtime code for contract B|runtime code for contract C| *)
 
     (* And then, the runtime code for a particular contract is organized like this: *)
-    (* |dispatcher that jumps into somewhere|runtime code for case f|runtime code for case g| *)
+    (* |dispatcher that jumps into a runtime case|runtime code for case f|runtime code for case g| *)
 
     (* numbers about the storage *)
     (* The storage during the runtime looks like this: *)
-    (* |pc of the current contract|pod contract argument0|pod contract argument1| *)
+    (* |current pc (zero means none)|current contract_id|pod contract argument0|pod contract argument1| *)
     (* In addition, array elements are placed at the same location as in Solidity *)
 
   ; storage_contract_switcher_index : int
