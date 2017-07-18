58d28ee1cbdcf73eb7cc84173870e9e001b15b6e
Yoichi Hirai <i@yoichihirai.com>
2017-07-18 14:56:17 +0200

    Add some tests about keccak_hex

--- src/lib/lib_test.ml
+++ src/lib/lib_test.ml
@@ -22,4 +22,7 @@ let _ =
   let () = assert (Ethereum.case_header_signature_hash case1_case = (snd case0)) in
   let () = assert ((Ethereum.case_header_signature_hash case2_case) = case2_hash) in
   let () = assert (Big_int.eq_big_int (Ethereum.hex_to_big_int "01") Big_int.unit_big_int) in
+  let () = assert (Ethereum.string_keccak "" = Ethereum.hex_keccak "0x") in
+  let () = assert (Ethereum.string_keccak "a" = Ethereum.hex_keccak "0x61") in
+  let () = assert (Ethereum.string_keccak "ab" = Ethereum.hex_keccak "6162") in
   Printf.printf "lib_test: success\n"
