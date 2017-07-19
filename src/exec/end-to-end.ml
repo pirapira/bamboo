c52f29f11e7f7e4634981a012e0e90e020ae6dc2
Yoichi Hirai <i@yoichihirai.com>
2017-07-19 16:47:50 +0200

    Testing 014

--- src/exec/end-to-end.ml
+++ src/exec/end-to-end.ml
@@ -590,6 +590,27 @@ let testing_013 s acc =
   let () = assert (answer = "0x0000000000000000000000000000000000000000000000000000000000000001") in
   ()
 
+let testing_014 s acc =
+  let initcode_compiled : string = CompileFile.compile_file "./src/parse/examples/014_ifelse.bbo" in
+  let my_acc = reset_chain s (Some acc) in
+  let receipt = deploy_code s my_acc initcode_compiled in
+  let contract_address = receipt.contractAddress in
+  let deployed = eth_getCode s contract_address in
+  let () = assert (String.length deployed > 2) in
+  let () = Printf.printf "saw code!\n" in
+  let c : eth_transaction =
+    { from = my_acc
+    ; _to = contract_address
+    ; gas = "0x0000000000000000000000000000000000000000000000000000000005f5e100"
+    ; value = "0"
+    ; data = (compute_signature_hash "f(bool,bool)") ^ "00000000000000000000000000000000000000000000000000000000000000" ^ "00000000000000000000000000000000000000000000000000000000000001"
+    ; gasprice = "0x00000000000000000000000000000000000000000000000000005af3107a4000"
+    } in
+  let answer = eth_call s c in
+  let () = Printf.printf "got answer: %s\n%!" answer in
+  let () = assert (answer = "0x0000000000000000000000000000000000000000000000000000000000000000") in
+  ()
+
 let () =
   let s = Utils.open_connection_unix_fd filename in
   let my_acc = constructor_arg_test s in
@@ -602,6 +623,7 @@ let () =
   let () = testing_00i s my_acc in
   let () = testing_011 s my_acc in
   let () = testing_013 s my_acc in
+  let () = testing_014 s my_acc in
   let () = Unix.close s in
   ()
 
