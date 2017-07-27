384b68f5741e33253957d895a240ed0e4ff0adad
Yoichi Hirai <i@yoichihirai.com>
2017-07-27 17:36:37 +0200

    Use functions from batteries

--- src/lib/misc.ml
+++ src/lib/misc.ml
@@ -7,32 +7,13 @@ let rec first_some f lst =
      | Some x -> Some x
      end
 
-let option_map f a =
-  match a with
-  | Some x -> Some (f x)
-  | None -> None
-
 let rec change_first f lst =
   match lst with
   | [] -> None
   | h :: t ->
      begin match f h with
      | None ->
-       option_map (fun rest -> h :: rest)
+       BatOption.map (fun rest -> h :: rest)
                   (change_first f t)
      | Some n -> Some (n :: t)
      end
-
-let rec int_sum (lst : int list) =
-  match lst with
-  | [] -> 0
-  | h :: t ->
-     h + (int_sum t)
-
-let rec filter_map f lst =
-  match lst with
-  | [] -> []
-  | h :: t ->
-     match f h with
-     | Some h' -> h' :: (filter_map f t)
-     | None -> filter_map f t
