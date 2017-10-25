b282622545e9eae6aa27b765b37320b7a31f1a2d
Yoichi Hirai <i@yoichihirai.com>
2017-10-25 18:41:19 +0200

    Fix a mistake in how to call the function

--- src/exec/endToEnd.ml
+++ src/exec/endToEnd.ml
@@ -995,7 +995,7 @@ let testing_024 channel my_acc =
     ; _to = contract_address
     ; gas = "0x0000000000000000000000000000000000000000000000000000000005f5e100"
     ; value = "0"
-    ; data = (Ethereum.compute_signature_hash "unvault(uint256)")^
+    ; data = (Ethereum.compute_signature_hash "unvault(uint256,address)")^
                (pad_to_word "50")^(address_as_word hot)
     ; gasprice = "0x0000000000000000000000000000000000000000000000000000005af3107a40"
     } in
